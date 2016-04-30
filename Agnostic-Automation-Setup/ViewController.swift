52726a6276fed76ded2cdd649420f825a40715c0
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-04-30 14:15:05 +1000

    Xcode template project

---
+++ Agnostic-Automation-Setup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Agnostic-Automation-Setup
+//
+//  Created by Giovanni on 30/04/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
